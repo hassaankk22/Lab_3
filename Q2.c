49fd47a31a4cad7a4333cdfbcaa4889f8fb0d0de
hassaankk22 <hassaankk22@gmail.com>
2026-09-09 22:41:56 +0500

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -4,19 +4,19 @@ int main()
 {
 	char name[23];
 	printf("Enter Your Name: \n");
-	scanf("%s", &name);
+	scanf("%s", name);
 	int age;
 	printf("Enter your age: \n");
 	scanf("%d", &age);
 	char depart[100];
 	printf("Enter Your Department: \n");
-	scanf("%s", &depart);
+	scanf("%s", depart);
 	char std_id[100];
 	printf("Enter Your Student ID: \n");
-	scanf("%s", &std_id);
+	scanf("%s", std_id);
 	char uni[100];
 	printf("Enter Your University: \n");
-	scanf("%s", &uni);
+	scanf("%s", uni);
 	printf("Name: %s \nAge: %d \nDepartment: %s \nStudent ID: %s \nUniversity: %s \n ", name,age,depart,std_id,uni);
 	
 	
